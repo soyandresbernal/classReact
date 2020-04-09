96c53c135f039adb99721ccfd64ad158280da0a6
soyandresbernal <soyandresbernal@gmail.com>
2020-04-09 15:52:05 -0500

    list -> Map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React, { Component } from "react";
-import ConditionalSection from "./sections/conditionals";
+import React, { Component } from 'react';
+
 
 class App extends Component {
   render() {
-    return <ConditionalSection />;
+    const numbers = [1, 1, 3, 4, 5]
+    return (
+      <div>
+        <h4>List</h4>
+        {numbers.map((number, index) => {
+          return <p key={index}>I'm number {number}</p>
+        })}
+      </div>
+    );
   }
 }
-export default App;
+export default App
