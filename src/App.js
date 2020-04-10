382914f19030ba10ac96284b3df3d3794f97e9c9
soyandresbernal <soyandresbernal@gmail.com>
2020-04-09 19:12:20 -0500

    forms

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import React, { Component } from 'react';
-import cars from './data/cars.json';
+import Form from './sections/forms'
 
-class CarItem extends Component {
-  render() {
-    const { car, id } = this.props
-    return (
-      <li>
-        <p>Key:{id}</p>
-        <p>{car.name}</p>
-        <p>{car.company}y</p>
-      </li>
-    );
-  }
-}
 
 
 export default class CarsList extends Component {
   render() {
     return (
-      <li>
-        {
-          cars.map(car => {
-            return <CarItem
-              id={car.id}
-              key={car.id}
-              car={car}
-            />
-          })
-        }
-      </li>
+
+      <Form />
+
+
     );
   }
 }
